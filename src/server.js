b72cb35769b552a7a50e437ccb469651f0f66305
Adrian Botello <adrianbotellog01@gmail.com>
2025-04-18 22:03:05 -0600

    update: testing new endpoints

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const cors = require('cors')
 
+const ArachnidaRouter = require("./routes/arachnida")
+const InsectaRouter = require("./routes/insecta")
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/arachnida/', ArachnidaRouter)
+app.use('/insecta/', InsectaRouter)
 
 app.get('/', (req,res) => {
     res.send("Hola desde Docker")
