a89c3eb40196f627cd2deb6d6d192334729b884d
Adrian Botello <adrianbotellog01@gmail.com>
2025-04-18 22:01:36 -0600

    chore: insecta and arachnida

---
+++ src/schema/arachnida.js
@@ -0,0 +1,34 @@
+export const arachnidaSchema = {
+    tax: {
+        kingdom: "Animalia",
+        phylum: "Arthropoda",
+        class: "Arachnida",
+        order: "",
+    },
+
+    name: {
+        common: "",
+        scientific: "",
+        others: [],
+    },
+
+    behavior: {
+        habitat: "",
+        diet: "",
+        role: "",
+    },
+
+    info: {
+        plague: false,
+        distribution: [],
+        images: [],
+        conservationStatus: "",
+    },
+
+    medical: {
+        venomous: false,
+        sympthoms: [],
+    },
+
+    sources: []
+}
---
+++ src/schema/insecta.js
@@ -0,0 +1,34 @@
+export const insectaSchema = {
+    tax: {
+        kingdom: "Animalia",
+        phylum: "Arthropoda",
+        class: "Insecta",
+        order: "",
+    },
+
+    name: {
+        common: "",
+        scientific: "",
+        others: [],
+    },
+
+    behavior: {
+        habitat: "",
+        diet: "",
+        role: "",
+    },
+
+    info: {
+        plague: false,
+        distribution: [],
+        images: [],
+        conservationStatus: "",
+    },
+
+    medical: {
+        venomous: false,
+        sympthoms: [],
+    },
+
+    sources: []
+}
